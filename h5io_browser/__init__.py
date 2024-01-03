e462f1c45c226023033891532fc789092d6aa4b0
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-01-03 07:22:44 +0100

    Update __init__.py

--- h5io_browser/__init__.py
+++ h5io_browser/__init__.py
@@ -1,3 +1,13 @@
+# fmt: off
+from h5io_browser.pointer import Pointer
+from h5io_browser.base import (
+    delete_item,
+    list_hdf,
+    read_dict_from_hdf,
+    write_dict_to_hdf,
+)
+# fmt: on
+
 from ._version import get_versions
 
 # Set version of h5io_browser
