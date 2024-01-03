479fd08ecef22035e034c04c7bf1cdbaaeb9514a
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-01-03 07:20:47 +0100

    Update __init__.py

--- h5io_browser/__init__.py
+++ h5io_browser/__init__.py
@@ -1,3 +1,11 @@
+from h5io_browser.pointer import Pointer  # noqa, analysis:ignore
+from h5io_browser.base import (  # noqa, analysis:ignore
+    delete_item,  # noqa, analysis:ignore
+    list_hdf,  # noqa, analysis:ignore
+    read_dict_from_hdf,  # noqa, analysis:ignore
+    write_dict_to_hdf,  # noqa, analysis:ignore
+)  # noqa, analysis:ignore
+
 from ._version import get_versions
 
 # Set version of h5io_browser
