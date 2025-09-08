24d98e4d15e9092408f4455ce4e2ec1984915764
Jan Janssen <janssen@mpie.de>
2025-09-08 17:34:04 +0200

    warn when the file changes since the future was initialized

--- h5io_browser/future.py
+++ h5io_browser/future.py
@@ -1,17 +1,27 @@
 from concurrent.futures import Future
 
+import os
+import warnings
+from typing import Optional
+
 from h5io_browser.base import list_hdf, read_dict_from_hdf
 
 
 class HDFFuture(Future):
-    def __init__(self, file_name, h5_path):
+    def __init__(self, file_name: str, h5_path: str, file_time_stamp: Optional[float]=None):
         super().__init__()
         self._file_name = file_name
         self._h5_path = h5_path
         self._hdf_read = False
+        if file_time_stamp is None:
+            self._file_time_stamp = os.stat(file_name).st_mtime
+        else:
+            self._file_time_stamp = file_time_stamp
 
     def _load_from_hdf(self):
         if not self._hdf_read:
+            if os.stat(self._file_name).st_mtime != self._file_time_stamp:
+                warnings.warn("The HDF5 file has been modified since the Future object was initialized: {}".format(self._file_name))
             try:
                 self.set_result(
                     read_dict_from_hdf(
@@ -50,7 +60,8 @@ def read_future_dict_from_hdf(
     Returns:
         dict: The loaded data as nested dictionary with the results being represented as Future objects.
     """
+    file_time_stamp = os.stat(file_name).st_mtime
     return {
-        k.replace(h5_path + "/", ""): HDFFuture(file_name=file_name, h5_path=k)
+        k.replace(h5_path + "/", ""): HDFFuture(file_name=file_name, h5_path=k, file_time_stamp=file_time_stamp)
         for k in list_hdf(file_name=file_name, h5_path=h5_path, recursive=recursive)[0]
     }
